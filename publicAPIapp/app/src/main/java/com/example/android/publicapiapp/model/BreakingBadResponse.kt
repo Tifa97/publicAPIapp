2dccd34d94cb360576d4aa29b0abb4dbd6d5b274
kristian <kristian.radic@masinerija.com>
2020-09-07 14:59:42 +0200

    start + BreakingBadResponse klasa

---
+++ publicAPIapp/app/src/main/java/com/example/android/publicapiapp/model/BreakingBadResponse.kt
@@ -0,0 +1,8 @@
+package com.example.android.publicapiapp.model
+
+data class BreakingBadResponse(
+    val characters: String,
+    val deaths: String,
+    val episodes: String,
+    val quotes: String
+)
