9cc1a32c98aac56de3c20f853dc8e07dbccc4ad8
kristian <kristian.radic@masinerija.com>
2020-09-07 16:26:15 +0200

    no message

---
+++ publicAPIapp/app/src/main/java/com/example/android/publicapiapp/adapter/CharactersAdapter.kt
@@ -0,0 +1,12 @@
+package com.example.android.publicapiapp.adapter
+
+import android.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.android.publicapiapp.databinding.ActivityMainBinding
+import com.example.android.publicapiapp.model.BreakingBadCharacterItem
+
+class CharactersAdapter {
+
+
+}
+
