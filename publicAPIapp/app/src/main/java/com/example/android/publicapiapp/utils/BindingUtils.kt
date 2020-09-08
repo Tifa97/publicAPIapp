5e14e86a63a432b0b2efc87c9f3132f2892753ba
kristian <kristian.radic@masinerija.com>
2020-09-08 08:01:34 +0200

    character list item

---
+++ publicAPIapp/app/src/main/java/com/example/android/publicapiapp/utils/BindingUtils.kt
@@ -0,0 +1,26 @@
+package com.example.android.publicapiapp.utils
+
+import android.widget.TextView
+import androidx.databinding.BindingAdapter
+import com.example.android.publicapiapp.model.BreakingBadCharacterItem
+
+@BindingAdapter("nameString")
+fun TextView.setName(character: BreakingBadCharacterItem){
+    character?.let {
+        text = character.name
+    }
+}
+
+@BindingAdapter("nicknameString")
+fun TextView.setNickname(character: BreakingBadCharacterItem){
+    character?.let {
+        text = character.nickname
+    }
+}
+
+@BindingAdapter("portrayedString")
+fun TextView.setPortrayed(character: BreakingBadCharacterItem){
+    character?.let {
+        text = character.portrayed
+    }
+}
