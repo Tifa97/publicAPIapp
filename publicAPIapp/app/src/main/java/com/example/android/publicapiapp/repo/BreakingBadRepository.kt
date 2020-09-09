eabb48efc97ee70f6d2c791fdcba31bdd559d2a2
kristian <kristian.radic@masinerija.com>
2020-09-09 10:19:35 +0200

    repository s mapperom

--- publicAPIapp/app/src/main/java/com/example/android/publicapiapp/repo/BreakingBadRepository.kt
+++ publicAPIapp/app/src/main/java/com/example/android/publicapiapp/repo/BreakingBadRepository.kt
@@ -2,11 +2,24 @@ package com.example.android.publicapiapp.repo
 
 import com.example.android.publicapiapp.api.APIClient
 import com.example.android.publicapiapp.api.ApiInterface
+import com.example.android.publicapiapp.model.CharacterObject
+import com.example.android.publicapiapp.repo.mapper.ResponseMapper
 
 
 //Repository ne smije nista primati
 class BreakingBadRepository(){
     val services by lazy { APIClient.authServices() }
 
-    fun getCharacter() = services.getCharacter()
+    suspend fun getCharacter(): List<CharacterObject>{
+        val result = services.getCharacters()
+
+        var returnList = mutableListOf<CharacterObject>()
+        if (result.isSuccessful){
+            result.body()?.let {list ->
+                returnList = ResponseMapper().getCharacters(list)
+            }
+        }
+
+        return returnList.toList()
+    }
 }
